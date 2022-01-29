0de688ec3e06387e6d89cb40522167333577d33d
Yaumama <83031256+Yaumama@users.noreply.github.com>
2022-01-29 09:19:14 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Player {
+    public string abilitySet = "";
+    public string name = "";
+    
+    public void useAbility() {
+        if (abilitySet == "fire") {
+            Console.WriteLine(name + " used fireball which did 40 damage!");
+        }
+    }
+}
+
+class Match {
+    public int PlayerCount;
+    public Player[] players;
+
+    public Match(int count) {
+        PlayerCount = count;
+        players = new Player[PlayerCount];
+    }
+
+    public void Start() {
+        Console.WriteLine("Game started! There are " + PlayerCount + " players!");
+    }
+}
+
+class Program {
+    public static void Main(string[] args) {
+
+        Console.WriteLine("How many players are there?");
+
+        string? playerCount = Console.ReadLine();
+
+        if (Int32.TryParse(playerCount, out int j)) {
+            
+            Console.WriteLine("You have selected " + playerCount + " players!");
+
+            Match match = new Match(j);
+
+            for (int i = 0; i < j; i++) {
+                Player plr = new Player();
+
+                match.players[i] = plr;
+
+                plr.name = "Player " + (i + 1);
+
+                Console.WriteLine("What do you want " + plr.name + "'s ability set to be?");
+
+                string? ability = Console.ReadLine();
+
+                if (ability != null) {
+                    if (ability.ToLower() == "fire") {
+                        Console.WriteLine(plr.name + " has " + ability + "!");
+                        plr.abilitySet = ability;
+                    } else {
+                        Console.WriteLine("That is not a ability!");
+                    }
+                }
+            }
+        } else {
+            Console.WriteLine("Please enter a number of players not text!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+#
+# Text Based Battle Game!
+
+## A WIP project by Yaumama
+<br>
+This is very much work in progress and is gonna be changed in the future.  
+
+Features right now:  
+
+> Player system with abilities  
+
+> Playercount  
+
+More will be added in the future!
+
+# Thanks for visiting!
+
+This is my first repo so thanks for visiting it and consider giving feedback!
